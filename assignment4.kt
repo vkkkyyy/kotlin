1fd4def78a2707001ec9d063d8c49256198b3203
vkkkyyy <githinjivictoria06@gmail.com>
2026-03-27 22:17:38 +0300

    added files

--- assignment4.kt
+++ assignment4.kt
@@ -1,6 +1,30 @@
-fun main() {
+;fun main() {
     //create a function that accepts parameter that is able to do convertion of degree celsius into fahrenheit
-    fun celsiusToFahrenheit(celsius:Double):
+    fun celsiusToFahrenheit(celsius:Double): Double{
+        return (celsius * 1.8)+32
+    }
+    val fahrenheit = celsiusToFahrenheit(16.2)
+    println(fahrenheit)
+    println("_________________")
+
+
 // create a function that is able to calculate the compound interest of a person
-//research on inheritance and give examples of a program
+fun compound(p: Double, r: Double, t: Double, n: Int) = p * Math.pow(1 + r/n, n * t)
+// Instead of using curly braces { } and a return keyword, the = tells Kotlin: "The result of this function is whatever this math equation produces."
+val result = compound(8900.0 , 0.05 , 10.5 , 12)
+val formattedResult = "%.2f".format(result)
+println("$formattedResult")
+//"%.2f": This is a special "template."
+//% starts the command.
+//.2 specifies two digits after the decimal.
+//f stands for "floating-point number" (a decimal).
+  println("_________________")
+
+
+
+
+
+
+
+
 }
---
+++ assignment4b.kt
@@ -0,0 +1,28 @@
+//research on inheritance and give examples of a program
+//In programming, inheritance is a mechanism where one class (the child) takes on the properties and behaviors (fields and methods) of another class (the parent). 
+
+//KEY RULES IN KOTLIN
+//open keyword: By default, Kotlin classes are "final" (closed). You must mark a class as open to allow others to inherit from it.
+//: operator: Used to indicate which class is being inherited.
+//override keyword: Required when a child class redefines a method from the parent. 
+// 1. Parent Class (The General Group)
+// We use 'open' so other classes CAN inherit from it
+open class Animal {
+    fun eat() {
+        println("This animal is eating.")
+    }
+}
+
+// 2. Child Class (The Specific Group)
+// The ':' means Dog "is an" Animal
+class Dog : Animal() {
+    fun bark() {
+        println("The dog says: Woof!")
+    }
+}
+fun main(){
+     val myDog = Dog()
+
+    myDog.eat()  // This comes from the Parent (Animal)
+    myDog.bark() // This is unique to the Child (Dog)
+}
